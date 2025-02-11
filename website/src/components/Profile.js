d3ad5c82e4b39520408077f41972bb9ba028d90b
chase <chasevitale@10-21-144-102.dynapool.wireless.nyu.edu>
2025-02-11 00:00:24 -0500

    rm econ

--- website/src/components/Profile.js
+++ website/src/components/Profile.js
@@ -18,7 +18,7 @@ const Profile = () => {
 
                 {/* Large Text */}
                 <p className="bodyText">
-                    My name is Chase! I'm currently a student enrolled at <span className="purpleText">NYU</span> studying Mathematics and Computer Science, with a minor in Economics. My passion for problem solving has been constant throughout my academic journey, inspiring me to study these two fields. Mathematics has developed my critical thinking skills, allowing me to approach challenges more precisely. Computer Science has given me the tools to implement my own project ideas, creating real, tangible products.
+                    My name is Chase! I'm currently a student enrolled at <span className="purpleText">NYU</span> studying Mathematics and Computer Science. My passion for problem solving has been constant throughout my academic journey, inspiring me to study these two fields. Mathematics has developed my critical thinking skills, allowing me to approach challenges more precisely. Computer Science has given me the tools to implement my own project ideas, creating real, tangible products.
                     <br></br>
                     <br></br>
                     <span className="purpleText">NYU</span>'s courses have provided me essential programming skills, such as data management, logical analysis, and manipulating numerical data. Outside of this, my personal projects have developed my skills in UI/UX design, app and web development, and backend storage of data.
